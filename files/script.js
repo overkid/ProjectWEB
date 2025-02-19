3bbed5648dc59ce321d7211dbf8ac309739ea8ac
overdosekid <102940151+overkid@users.noreply.github.com>
2025-02-19 20:44:42 +0300

    еще анимки какиет

--- files/script.js
+++ files/script.js
@@ -21,3 +21,58 @@ window.addEventListener("scroll", () => {
     document.querySelector(".main-photo2").style.transform = `translateY(${scrollTop * -0.4}px)`;
 });
 
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const sections = document.querySelectorAll(".gallery-content");
+  
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            gsap.to(entry.target, { opacity: 1, y: 0, duration: 0.4 });
+            observer.unobserve(entry.target); // Чтобы не повторялось
+          }
+        });
+      },
+      { threshold: 0.5 }
+    );
+  
+    sections.forEach((section) => observer.observe(section));
+  });
+
+  document.addEventListener("DOMContentLoaded", () => {
+    const sections = document.querySelectorAll(".features-content");
+  
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            gsap.to(entry.target, { opacity: 1, y: 0, duration: 0.4 });
+            observer.unobserve(entry.target); // Чтобы не повторялось
+          }
+        });
+      },
+      { threshold: 0.5 }
+    );
+  
+    sections.forEach((section) => observer.observe(section));
+  });
+
+  document.addEventListener("DOMContentLoaded", () => {
+    const sections = document.querySelectorAll(".cta1, .cta2");
+  
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            gsap.to(entry.target, { opacity: 1, y: 0, duration: 0.4 });
+            observer.unobserve(entry.target); // Чтобы не повторялось
+          }
+        });
+      },
+      { threshold: 0.5 }
+    );
+  
+    sections.forEach((section) => observer.observe(section));
+  });
